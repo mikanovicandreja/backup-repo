17a234c7e0371729f2ac9c9cacfa9133d7b2d019
Andreja Mikanovic <mikanovic.andreja@gmail.com>
2025-09-10 22:10:04 +0200

    All project files

---
+++ __main__.py
@@ -0,0 +1,29 @@
+"""A Python Pulumi program"""
+
+import os
+
+import pulumi
+import pulumi_aws as aws
+import mimetypes
+
+config = pulumi.Config()
+site_dir = config.require("siteDir")
+
+
+bucket = aws.s3.Bucket("my-bucket", 
+    website={
+        "index_document": "index.html"
+})
+
+for file in os.listdir(site_dir):
+    filepath = os.path.join(site_dir, file)
+    mime_type, _ = mimetypes.guess_type(filepath)
+    obj = aws.s3.BucketObject(file, 
+        bucket=bucket.bucket,
+        source=pulumi.FileAsset(filepath),
+        acl="public-read",
+        content_type=mime_type
+    )
+
+pulumi.export('bucket_name', bucket.bucket)
+pulumi.export('bucket_endpoint', pulumi.Output.concat("https://", bucket.website_endpoint))
